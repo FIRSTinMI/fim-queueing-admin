c33482432cea7f9d12e690d3c11689002960fdda
Evan Lihou <e@evandoes.dev>
2023-02-27 10:42:45 -0500

    Fix bug in event index page

---
+++ Models/DbEvent.cs
@@ -0,0 +1,13 @@
+// ReSharper disable InconsistentNaming Intentional to match casing of DB model
+namespace fim_queueing_admin.Models;
+
+public class DbEvent
+{
+    public string? name { get; set; }
+    public string? eventCode { get; set; }
+    public int? field { get; set; }
+    public DateTime? start { get; set; }
+    public DateTime? end { get; set; }
+    public string? streamEmbedLink { get; set; }
+    public EventState? state { get; set; }
+}
