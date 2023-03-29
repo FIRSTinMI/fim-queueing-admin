6c504babc71071d29f4d132766c4ea14d1026f20
Evan Lihou <e@evandoes.dev>
2023-03-29 16:21:39 -0400

    Add video stats for active events
    At some point I'll add the info to the management page of non-active events

---
+++ Services/MatchVideosService.cs
@@ -0,0 +1,55 @@
+namespace fim_queueing_admin.Services;
+
+public class MatchVideosService : IService
+{
+    private readonly HttpClient _frcClient;
+
+    public MatchVideosService(IHttpClientFactory hcf)
+    {
+        _frcClient = hcf.CreateClient("FRC");
+    }
+    
+    public async Task<Dictionary<string, MatchVideosModel>> GetVideosForEvents(string season, IEnumerable<string> events)
+    {
+        var models = await Task.WhenAll(events.Select(x => GetVideosForEvent(season, x)));
+        return new Dictionary<string, MatchVideosModel>(models);
+    }
+
+    private async Task<KeyValuePair<string, MatchVideosModel>> GetVideosForEvent(string season, string evt)
+    {
+        var outModel = new MatchVideosModel();
+        await Task.WhenAll(new List<Task> {
+            Task.Run(async () =>
+            {
+                var matches = await _frcClient.GetFromJsonAsync<MatchResponse>($"{season}/matches/{evt}?tournamentLevel=qual");
+                outModel.QualMatchesTotal = matches?.Matches?.Count();
+                outModel.QualVideosAvailable = matches?.Matches?.Count(x => !string.IsNullOrEmpty(x.MatchVideoLink));
+            }),
+            Task.Run(async () =>
+            {
+                var matches = await _frcClient.GetFromJsonAsync<MatchResponse>($"{season}/matches/{evt}?tournamentLevel=playoff");
+                outModel.PlayoffVideosTotal = matches?.Matches?.Count();
+                outModel.PlayoffVideosAvailable = matches?.Matches?.Count(x => !string.IsNullOrEmpty(x.MatchVideoLink));
+            })
+        });
+
+        return new KeyValuePair<string, MatchVideosModel>(evt, outModel);
+    }
+
+    public class MatchVideosModel
+    {
+        public int? QualVideosAvailable { get; set; }
+        public int? QualMatchesTotal { get; set; }
+        public int? PlayoffVideosAvailable { get; set; }
+        public int? PlayoffVideosTotal { get; set; }
+    }
+
+    public class MatchResponse
+    {
+        public IEnumerable<Match>? Matches { get; set; }
+        public class Match
+        {
+            public string? MatchVideoLink { get; set; }
+        }
+    }
+}
